57feafa346dbdae7c2d0131d6051cd376d1da441
YuHansage <161785734+YuHansage@users.noreply.github.com>
2024-04-01 15:37:53 +0800

    first commit

---
+++ css/data.js
@@ -0,0 +1,7 @@
+let song [
+    {
+        name:'song 1',
+        path:'music/song 1. mp3',
+        
+    }
+]
---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: 'Bling-Bang-Bang-Born',
+        path: 'music/Song 1.mp3',
+        artist: 'Creepy Nuts',
+        cover: 'img/cover 1.png'
+    },
+    {
+        name: '晩餐歌',
+        path: 'music/Song 2.mp3',
+        artist: 'tuki.',
+        cover: 'img/cover 2.png'
+    },
+]
